91c407fdc14a534b17f40d3442c4a8a4abb0a8dd
mercenarioZ <funnystar320@gmail.com>
2026-07-17 10:46:24 +0700

    chore: add test for web_search

---
+++ search_test.go
@@ -0,0 +1,30 @@
+package main
+
+import "testing"
+
+func TestWebSearchReq_RequiresWebSearch(t *testing.T) {
+	request := buildWebSearchReq(
+		"gpt-5.6",
+		"latest Go release",
+	)
+
+	if request.Model != "gpt-5.6" {
+		t.Errorf("got model %q", request.Model)
+	}
+
+	if request.Input != "latest Go release" {
+		t.Errorf("got input %q", request.Input)
+	}
+
+	if len(request.Tools) != 1 {
+		t.Fatalf("got %d tools, want 1", len(request.Tools))
+	}
+
+	if request.Tools[0] != "web_search" {
+		t.Errorf("got tool %q, want web_search", request.Tools[0].Type)
+	}
+
+	if request.ToolChoice != "required" {
+		t.Errorf("got tool choice %q, want required", request.ToolChoice)
+	}
+}
